4348ce3cdd30b3466c50b7cf86c611d6f2e68237
Marcelo Salinas <chelo336@gmail.com>
2022-04-10 23:32:37 -0400

    actionprimeraPruebVacas

--- src/VacasToros.test.js
+++ src/VacasToros.test.js
@@ -2,6 +2,6 @@ import codSecret from "./VacasToros.js";
 
 describe("Vacas", () => {
   it("deberia devolver *", () => {
-    expect(codSecret("1", "1")).toEqual("*");
+    expect(codSecret("2", "2")).toEqual("*");
   });
 });
