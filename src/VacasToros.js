8eeb1bbd22e3b66663154d70a3775f71e18f5cba
Marcelo Salinas <chelo336@gmail.com>
2022-04-12 00:01:49 -0400

    añadiendoFuncionVacasToros

--- src/VacasToros.js
+++ src/VacasToros.js
@@ -2,7 +2,7 @@ function Vacas(list,cod){
     var res=[];
     var list2=list.split(",");
     var cod2=cod.split(",");
-    for(let i=1;i<=list2.length;i++){
+    for(let i=0;i<list2.length;i++){
         if(list2[i]==cod2[i]){
             res+= "*";
         }
@@ -30,5 +30,12 @@ function Toros(list,cod){
     return res;
     
 }
+function VacasyToros(list,cod){
+    var res1=[],res2=[],Res=[];
+    res1+=Vacas(list,cod);
+    res2+=Toros(list,cod);
+    Res=res1.concat(res2);
+    return Res;
+}
 
-export default Toros;
+export default VacasyToros;
--- src/VacasToros.test.js
+++ src/VacasToros.test.js
@@ -1,16 +1,16 @@
-import Toros from "./VacasToros.js";
+import VacasyToros from "./VacasToros.js";
 
 describe("Vacas", () => {
   it("deberia devolver *", () => {
-    expect(Vacas("2", "2")).toEqual("*");
+    expect(VacasyToros("2", "2")).toEqual("*");
   });
   it("deberia devolver * *", () => {
-    expect(Vacas("2,3", "2,3")).toEqual("**");
+    expect(VacasyToros("2,3", "2,3")).toEqual("**");
   });
   it("deberia devolver * * ", () => {
-    expect(Vacas("2,3,4", "2,3,5")).toEqual("**");
+    expect(VacasyToros("2,3,4", "2,3,5")).toEqual("**");
   });
   it("deberia devolver * * ! ", () => {
-    expect(Toros("3,2,5", "2,3,4")).toEqual("!!");
+    expect(VacasyToros("3,2,5", "2,3,4")).toEqual("!!");
   });
 });
